31ce48fe5537966d186427ae95183b4e6d047a63
Deepankar523 <76254464+Deepankar523@users.noreply.github.com>
2026-02-21 19:17:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from google import genai
+
+# 🔐 Put your NEW Gemini API key here
+client = genai.Client(api_key="AIzaSyAjbURRK2Aapv6NMSzWnAWR-o_LNtle7wE")
+
+st.title("📰 AI News Intelligence System")
+
+news_text = st.text_area("Paste News Article Here")
+
+if news_text:
+    prompt = f"""
+    Analyze the following news article and provide:
+
+    1. Sentiment (Positive / Negative / Neutral)
+    2. Category
+    3. Short Summary
+    4. 5 Keywords
+
+    Article:
+    {news_text}
+    """
+
+    response = client.models.generate_content(
+        model="gemini-1.5-flash-latest",
+        contents=prompt
+    )
+
+    st.subheader("AI Analysis Result")
+    st.write(response.text)
